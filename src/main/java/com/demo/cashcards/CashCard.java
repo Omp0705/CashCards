bdfeec6e38a95159e65015a255616a8ebafdd714
Om Patil <patilom4634@gmail.com>
2025-02-02 00:15:30 +0530

    Updated the files

--- src/main/java/com/demo/cashcards/CashCard.java
+++ src/main/java/com/demo/cashcards/CashCard.java
@@ -2,6 +2,7 @@ package com.demo.cashcards;
 
 import org.springframework.data.annotation.Id;
 
+// entitiy class 
 public record CashCard(@Id Long id, Double amount, String owner) {
     
 	public CashCard(Double amount, String owner) {
